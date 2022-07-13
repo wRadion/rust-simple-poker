adc8bc5d92a498038addc7465ff1a24781bd11f4
wRadion <wradion@gmail.com>
2022-07-13 15:41:52 +0200

    Removed cards crate target dir (oops)

--- cards/target/debug/build/syn-ba77fa66992860c0/build_script_build-ba77fa66992860c0.d
+++
@@ -1,5 +0,0 @@
-/home/wradion/rust/poker/cards/target/debug/build/syn-ba77fa66992860c0/build_script_build-ba77fa66992860c0: /home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.98/build.rs
-
-/home/wradion/rust/poker/cards/target/debug/build/syn-ba77fa66992860c0/build_script_build-ba77fa66992860c0.d: /home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.98/build.rs
-
-/home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.98/build.rs:
--- cards/target/debug/deps/cfg_if-904ac7e0155b8ce8.d
+++
@@ -1,7 +0,0 @@
-/home/wradion/rust/poker/cards/target/debug/deps/cfg_if-904ac7e0155b8ce8.rmeta: /home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/wradion/rust/poker/cards/target/debug/deps/libcfg_if-904ac7e0155b8ce8.rlib: /home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/wradion/rust/poker/cards/target/debug/deps/cfg_if-904ac7e0155b8ce8.d: /home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/wradion/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
